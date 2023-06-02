493e446e44299360159262a714bbbec073c47586
farooq89 <farooqusman89@gmail.com>
2023-06-02 15:33:14 +0500

    aws code updated

--- AWScred.tf
+++ AWScred.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  region = "eu-north-1"  # Set your desired AWS region
+  region = "eu-north-1"
   access_key = "AKIARVLWEW5R5O7EB6Q7"  
   secret_key = "c8a3FrEIhpgwin8eIIkZN6VTgsNcB8rEZ5E+kxDe"  
 }
-
-
