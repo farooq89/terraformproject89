2f4c82f13437329941e86a7fb07b9cd3075e1c6c
farooq89 <75387625+farooq89@users.noreply.github.com>
2023-06-02 14:52:21 +0500

    Update AWScred.tf

--- AWScred.tf
+++ AWScred.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-1"  # Set your desired AWS region
+  region = "eu-north-1"  # Set your desired AWS region
   access_key = "AKIARVLWEW5R5O7EB6Q7"  
   secret_key = "c8a3FrEIhpgwin8eIIkZN6VTgsNcB8rEZ5E+kxDe"  
 }
