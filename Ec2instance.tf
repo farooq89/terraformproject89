b258c5a4c0665d9c465470fa023aec705a0d28dd
farooq89 <farooqusman89@gmail.com>
2023-06-02 19:04:43 +0500

    ami

--- Ec2instance.tf
+++ Ec2instance.tf
@@ -3,15 +3,40 @@ provider "aws" {
   region = "us-east-1"
 }
 
+ resource "aws_security_group" "example" {
+  name        = "my-security-group"
+  description = "Example security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+} 
+
 
 resource "aws_instance" "ubuntu" {
-  ami           = "ami-053b0d53c279acc90"  
+  ami           = "ami-0261755bbcb8c4a84"  
   instance_type = "t2.micro"  # Set the desired instance type
 
   # Specify other instance configuration options as needed
   key_name      = "dotnetapp"
   subnet_id     = "subnet-0ef0c2b1ec9f62c79"
-  security_group_ids = ["ami-0261755bbcb8c4a84"]
 
   tags = {
     Name = "MyInstance"
