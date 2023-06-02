ddf6f10868289981f0be8560f12e962d65626638
farooq89 <farooqusman89@gmail.com>
2023-06-02 16:22:45 +0500

    ami

--- Ec2instance.tf
+++ Ec2instance.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99"  
+  ami           = "ami-0130c3a072f3832ff"  
   instance_type = "t2.micro"  # Set the desired instance type
 
   # Specify other instance configuration options as needed
