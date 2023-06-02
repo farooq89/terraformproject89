dec3c2d6cd3a8858ba88ad0f07272ed6311aa2f2
farooq89 <75387625+farooq89@users.noreply.github.com>
2023-06-02 15:26:20 +0500

    Update AWScred.tf

--- AWScred.tf
+++ AWScred.tf
@@ -5,21 +5,3 @@ provider "aws" {
 }
 
 
-
-provider "aws" {
-  region = "us-east-1"  # Set your desired AWS region
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-097d5b19d4f1a7d1b"  # Specify the ID of the Amazon Machine Image (AMI) you want to use
-  instance_type = "t2.micro"  # Set the desired instance type
-
-  # Specify other instance configuration options as needed
-  key_name      = "dotnetapp"
-  subnet_id     = "subnet-0ef0c2b1ec9f62c79"
-  security_group_ids = ["sg-0ccc6a0ae4562052e"]
-
-  tags = {
-    Name = "MyInstance"
-  }
-}
