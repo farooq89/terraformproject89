7293c4ed611468b7a78565cddacb2f9f36c4382c
farooq89 <75387625+farooq89@users.noreply.github.com>
2023-06-02 14:27:10 +0500

    Update AWScred.tf

--- AWScred.tf
+++ AWScred.tf
@@ -1,11 +1,11 @@
-variable “aws_access_key”{
-default = “AKIARVLWEW5R5O7EB6Q7”
+variable "aws_access_key"{
+default = "AKIARVLWEW5R5O7EB6Q7"
 }
 variable "aws_secret_key"{
 default = "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"
 }
 
-provider “aws” {
+provider "aws" {
 access_key=var.aws_access_key
 secret_key=var.aws_secret_key
 }
