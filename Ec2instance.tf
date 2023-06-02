7643b19b00fc59ed03b652396710998c4444b21c
farooq89 <farooqusman89@gmail.com>
2023-06-02 15:31:43 +0500

    ec2 code updated

---
+++ Ec2instance.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-097d5b19d4f1a7d1b"  
+  instance_type = "t2.micro"  # Set the desired instance type
+
+  # Specify other instance configuration options as needed
+  key_name      = "dotnetapp"
+  subnet_id     = "subnet-0ef0c2b1ec9f62c79"
+  security_group_ids = ["sg-0ccc6a0ae4562052e"]
+
+  tags = {
+    Name = "MyInstance"
+  }
+}
