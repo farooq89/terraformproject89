dd7af0bbf059181d71fd99962243761a68308e28
farooq89 <farooqusman89@gmail.com>
2023-06-02 17:24:20 +0500

    ami

--- Ec2instance.tf
+++ Ec2instance.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "ubuntu" {
   # Specify other instance configuration options as needed
   key_name      = "dotnetapp"
   subnet_id     = "subnet-0ef0c2b1ec9f62c79"
-  security_group_ids = ["sg-xxxxxxxx"]
+  security_group_ids = ["ami-0261755bbcb8c4a84"]
 
   tags = {
     Name = "MyInstance"
